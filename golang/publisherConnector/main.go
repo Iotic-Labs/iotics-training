a7b8a9d8598a66c2a73771988b0e736a983556da
Lorenzo Paris <lorenzo.paris@iotics.com>
2022-12-21 17:33:58 +0000

    Golang Training completed

---
+++ golang/publisherConnector/main.go
@@ -0,0 +1,354 @@
+package main
+
+import (
+	"fmt"
+	helpers "iotics-training/helpers"
+	"log"
+	"math/rand"
+	"net/url"
+	"time"
+
+	ioticsApi "github.com/Iotic-Labs/iotics-api-grpc-go/iotics/api"
+	"github.com/Iotic-Labs/iotics-identity-go/pkg/register"
+)
+
+const (
+	RESOLVER_URL  = ""
+	HOST_GRPC_URL = "" // <spacename>.iotics.com:10001
+
+	USER_KEY_NAME = ""
+	USER_SEED     = ""
+
+	AGENT_KEY_NAME = ""
+	AGENT_SEED     = ""
+)
+
+func main() {
+	resolverAddr, err := url.Parse(RESOLVER_URL)
+	if err != nil {
+		log.Fatalf("Could not parse RESOLVER_URL: %v", err)
+	}
+	resolverClient := register.NewDefaultRestResolverClient(resolverAddr)
+
+	userIdentity, agentIdentity := helpers.CreateUserAndAgentWithAuthDelegation(
+		resolverClient,
+		USER_SEED, USER_KEY_NAME,
+		AGENT_SEED, AGENT_KEY_NAME,
+	)
+
+	token := helpers.GetToken(agentIdentity, userIdentity, 600)
+	apiContext := helpers.GetApiContext(token, HOST_GRPC_URL, "Publisher Connector")
+
+	/*** CREATE TWIN TEMPERATURE MODEL WITH CONTROL DELEG ***/
+	/* Let's now use the HighLevel Identity library to create a new Twin Identity
+	   and perform the Control Delegation with a single operation.
+	   For the sake of this exercise, the Twin Model doesn't need any change to the AllowLists' (it can be not findable).
+	   We want to use the Upsert operation as it combines multiple operations into a single one */
+	twinTempModelIdentity := helpers.CreateTwinWithControlDelegation(resolverClient, AGENT_SEED, "TempModel", agentIdentity)
+
+	_, err = apiContext.TwinAPI.UpsertTwin(
+		apiContext.CtxWithMeta,
+		&ioticsApi.UpsertTwinRequest{
+			Headers: &apiContext.Headers,
+			Payload: &ioticsApi.UpsertTwinRequest_Payload{
+				TwinId: &ioticsApi.TwinID{Id: twinTempModelIdentity.Did()},
+				Properties: []*ioticsApi.Property{
+					{
+						Key: helpers.PROPERTY_KEY_TYPE,
+						Value: &ioticsApi.Property_UriValue{
+							UriValue: &ioticsApi.Uri{
+								Value: helpers.PROPERTY_VALUE_MODEL,
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_LABEL,
+						Value: &ioticsApi.Property_LangLiteralValue{
+							LangLiteralValue: &ioticsApi.LangLiteral{
+								Value: "Temperature Sensor Model - LP",
+								Lang:  "en",
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_COMMENT,
+						Value: &ioticsApi.Property_LangLiteralValue{
+							LangLiteralValue: &ioticsApi.LangLiteral{
+								Value: "Model of a Temperature Sensor Twin",
+								Lang:  "en",
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_SPACE_NAME,
+						Value: &ioticsApi.Property_StringLiteralValue{
+							StringLiteralValue: &ioticsApi.StringLiteral{
+								Value: "Space A",
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_COLOR,
+						Value: &ioticsApi.Property_StringLiteralValue{
+							StringLiteralValue: &ioticsApi.StringLiteral{
+								Value: "#9aceff",
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_CREATED_BY,
+						Value: &ioticsApi.Property_StringLiteralValue{
+							StringLiteralValue: &ioticsApi.StringLiteral{
+								Value: "Lorenzo",
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_DEFINES,
+						Value: &ioticsApi.Property_UriValue{
+							UriValue: &ioticsApi.Uri{
+								Value: helpers.PROPERTY_VALUE_SAREF_TEMPERATURE_SENSOR,
+							},
+						},
+					},
+					{
+						Key: helpers.PROPERTY_KEY_SAREF_HAS_MODEL,
+						Value: &ioticsApi.Property_StringLiteralValue{
+							StringLiteralValue: &ioticsApi.StringLiteral{
+								Value: "SET-LATER",
+							},
+						},
+					},
+				},
+				Feeds: []*ioticsApi.UpsertFeedWithMeta{
+					{
+						Id: "temperature",
+						Properties: []*ioticsApi.Property{
+							{
+								Key: helpers.PROPERTY_KEY_LABEL,
+								Value: &ioticsApi.Property_LangLiteralValue{
+									LangLiteralValue: &ioticsApi.LangLiteral{
+										Value: "Temperature",
+										Lang:  "en",
+									},
+								},
+							},
+							{
+								Key: helpers.PROPERTY_KEY_COMMENT,
+								Value: &ioticsApi.Property_LangLiteralValue{
+									LangLiteralValue: &ioticsApi.LangLiteral{
+										Value: "Current temperature of a Room",
+										Lang:  "en",
+									},
+								},
+							},
+						},
+						Values: []*ioticsApi.Value{
+							{
+								Label:    "reading",
+								Comment:  "Temperature in Celsius degrees",
+								DataType: "integer",
+								Unit:     helpers.UNIT_DEGREE_CELSIUS,
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalf("Could not Upsert Twin: %v", err)
+	}
+	log.Printf("Created Twin Model %s", twinTempModelIdentity.Did())
+
+	/*** CREATE 2 TWIN TEMPERATURE FROM THAT MODEL ***/
+	/* Let's copy-paste the entire Twin Model upsert info with the right changes:
+	   - twin_did
+	   - 1st Twin Property
+	   - Label
+	   - Comment
+	   - hostMetadataAllowList
+	   - hostAllowList.
+	   The Twins' setup phase ends here. */
+
+	// The following is mapping between Sensor ID and Twin DID (eventually we'll need the latter info to share data)
+	roomNumberToTwinId := make(map[int]string)
+
+	for roomNumber := 1; roomNumber <= 2; roomNumber++ {
+		twinTemperatureIdentity := helpers.CreateTwinWithControlDelegation(
+			resolverClient,
+			AGENT_SEED, fmt.Sprintf("Sensor%d", roomNumber),
+			agentIdentity,
+		)
+
+		_, err = apiContext.TwinAPI.UpsertTwin(
+			apiContext.CtxWithMeta,
+			&ioticsApi.UpsertTwinRequest{
+				Headers: &apiContext.Headers,
+				Payload: &ioticsApi.UpsertTwinRequest_Payload{
+					TwinId: &ioticsApi.TwinID{Id: twinTemperatureIdentity.Did()},
+					Properties: []*ioticsApi.Property{
+						{
+							Key: helpers.PROPERTY_KEY_FROM_MODEL,
+							Value: &ioticsApi.Property_UriValue{
+								UriValue: &ioticsApi.Uri{
+									Value: twinTempModelIdentity.Did(),
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_LABEL,
+							Value: &ioticsApi.Property_LangLiteralValue{
+								LangLiteralValue: &ioticsApi.LangLiteral{
+									Value: fmt.Sprintf("Temperature Sensor LP - Room %d", roomNumber),
+									Lang:  "en",
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_COMMENT,
+							Value: &ioticsApi.Property_LangLiteralValue{
+								LangLiteralValue: &ioticsApi.LangLiteral{
+									Value: fmt.Sprintf("Temperature Sensor Twin - Room %d", roomNumber),
+									Lang:  "en",
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_SPACE_NAME,
+							Value: &ioticsApi.Property_StringLiteralValue{
+								StringLiteralValue: &ioticsApi.StringLiteral{
+									Value: "Space A",
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_COLOR,
+							Value: &ioticsApi.Property_StringLiteralValue{
+								StringLiteralValue: &ioticsApi.StringLiteral{
+									Value: "#9aceff",
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_CREATED_BY,
+							Value: &ioticsApi.Property_StringLiteralValue{
+								StringLiteralValue: &ioticsApi.StringLiteral{
+									Value: "Lorenzo Paris",
+								},
+							},
+						},
+						// Saref TemperatureSensor Ontology
+						// Some of the properties of this ontology can be used to add more description to the Sensor
+						{
+							Key: helpers.PROPERTY_KEY_DEFINES,
+							Value: &ioticsApi.Property_UriValue{
+								UriValue: &ioticsApi.Uri{
+									Value: helpers.PROPERTY_VALUE_SAREF_TEMPERATURE_SENSOR,
+								},
+							},
+						},
+						// Add "hasModel" property
+						{
+							Key: helpers.PROPERTY_KEY_SAREF_HAS_MODEL,
+							Value: &ioticsApi.Property_StringLiteralValue{
+								StringLiteralValue: &ioticsApi.StringLiteral{Value: "T1234"},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_HOST_METADATA_ALLOW_LIST,
+							Value: &ioticsApi.Property_UriValue{
+								UriValue: &ioticsApi.Uri{
+									Value: helpers.PROPERTY_VALUE_ALLOW_ALL_HOSTS,
+								},
+							},
+						},
+						{
+							Key: helpers.PROPERTY_KEY_HOST_ALLOW_LIST,
+							Value: &ioticsApi.Property_UriValue{
+								UriValue: &ioticsApi.Uri{
+									Value: helpers.PROPERTY_VALUE_ALLOW_ALL_HOSTS,
+								},
+							},
+						},
+					},
+					Feeds: []*ioticsApi.UpsertFeedWithMeta{
+						{
+							Id: "temperature",
+							Properties: []*ioticsApi.Property{
+								{
+									Key: helpers.PROPERTY_KEY_LABEL,
+									Value: &ioticsApi.Property_LangLiteralValue{
+										LangLiteralValue: &ioticsApi.LangLiteral{
+											Value: "Temperature",
+											Lang:  "en",
+										},
+									},
+								},
+								{
+									Key: helpers.PROPERTY_KEY_COMMENT,
+									Value: &ioticsApi.Property_LangLiteralValue{
+										LangLiteralValue: &ioticsApi.LangLiteral{
+											Value: "Current temperature of a Room",
+											Lang:  "en",
+										},
+									},
+								},
+							},
+							Values: []*ioticsApi.Value{
+								{
+									Label:    "reading",
+									Comment:  "Temperature in Celsius degrees",
+									DataType: "integer",
+									Unit:     helpers.UNIT_DEGREE_CELSIUS,
+								},
+							},
+						},
+					},
+					Location: &ioticsApi.GeoLocation{
+						Lat: 51.864, Lon: -0.412,
+					},
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Could not Upsert Twin: %v", err)
+		}
+		log.Printf("Created Twin %s", twinTemperatureIdentity.Did())
+
+		// Update mapping
+		roomNumberToTwinId[roomNumber] = twinTemperatureIdentity.Did()
+	}
+
+	/*** SHARE DATA ***/
+	/* For each Twin we want to generate a random tenmperature and share the data */
+	for {
+		for roomNumber := range roomNumberToTwinId {
+			randTemp := 10 + rand.Intn(16)
+			messageToShare := fmt.Sprintf(`{"reading": %d}`, randTemp)
+			dataToShare := []byte(messageToShare)
+
+			apiContext.FeedAPI.ShareFeedData(
+				apiContext.CtxWithMeta,
+				&ioticsApi.ShareFeedDataRequest{
+					Headers: &apiContext.Headers,
+					Args: &ioticsApi.ShareFeedDataRequest_Arguments{
+						FeedId: &ioticsApi.FeedID{
+							Id:     "temperature",
+							TwinId: roomNumberToTwinId[roomNumber],
+						},
+					},
+					Payload: &ioticsApi.ShareFeedDataRequest_Payload{
+						Sample: &ioticsApi.FeedData{
+							Data: dataToShare,
+						},
+					},
+				},
+			)
+
+			log.Printf("Shared %s from Twin %s", messageToShare, roomNumberToTwinId[roomNumber])
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+}
---
+++ golang/synthesiserConnector/main.go
@@ -0,0 +1,397 @@
+package main
+
+import (
+	"encoding/json"
+	helpers "iotics-training/helpers"
+	"log"
+	"net/url"
+	"time"
+
+	ioticsApi "github.com/Iotic-Labs/iotics-api-grpc-go/iotics/api"
+	"github.com/Iotic-Labs/iotics-identity-go/pkg/register"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const (
+	RESOLVER_URL  = ""
+	HOST_GRPC_URL = "" // <spacename>.iotics.com:10001
+
+	USER_KEY_NAME = ""
+	USER_SEED     = ""
+
+	AGENT_KEY_NAME = ""
+	AGENT_SEED     = ""
+)
+
+/*** BONUS ***/
+// func getInputMessages(inputListener ioticsApi.InputAPI_ReceiveInputMessagesClient, inputValueLabel string, ch chan bool) {
+// 	log.Print("Waiting for Input Messages...")
+// 	mappedData := make(map[string]interface{})
+
+// 	for {
+// 		msg, err := inputListener.Recv()
+// 		if err != nil {
+// 			log.Printf("Error: %v", err)
+// 			break
+// 		}
+// 		msgReceived := msg.GetPayload().Message.Data
+// 		err = json.Unmarshal(msgReceived, &mappedData)
+// 		if err != nil {
+// 			log.Printf("Error: %v", err)
+// 			break
+// 		}
+
+// 		presence := bool(mappedData[inputValueLabel].(bool))
+// 		log.Printf("Received Input message: %t", presence)
+
+// 		if presence {
+// 			ch <- true
+// 		} else {
+// 			ch <- false
+// 		}
+// 	}
+// }
+
+func getFeedData(feedListener ioticsApi.InterestAPI_FetchInterestsClient, twinId string, feedValueLabel string, ch chan bool) {
+	log.Printf("Following Twin %s", twinId)
+	mappedData := make(map[string]interface{})
+
+	for {
+		latestFeedData, err := feedListener.Recv()
+		if err != nil {
+			log.Printf("Error: %v", err)
+			break
+		}
+
+		dataReceived := latestFeedData.GetPayload().FeedData.GetData()
+		err = json.Unmarshal(dataReceived, &mappedData)
+		if err != nil {
+			log.Printf("Error: %v", err)
+			break
+		}
+
+		temperature := int(mappedData[feedValueLabel].(float64))
+		log.Printf("Received Temperature: %d from Twin %s", temperature, twinId)
+
+		if temperature < 20 {
+			ch <- true
+		} else {
+			ch <- false
+		}
+	}
+}
+
+func main() {
+	resolverAddr, err := url.Parse(RESOLVER_URL)
+	if err != nil {
+		log.Fatalf("Could not parse RESOLVER_URL: %v", err)
+	}
+	resolverClient := register.NewDefaultRestResolverClient(resolverAddr)
+
+	userIdentity, agentIdentity := helpers.CreateUserAndAgentWithAuthDelegation(
+		resolverClient,
+		USER_SEED, USER_KEY_NAME,
+		AGENT_SEED, AGENT_KEY_NAME,
+	)
+
+	token := helpers.GetToken(agentIdentity, userIdentity, 600)
+	apiContext := helpers.GetApiContext(token, HOST_GRPC_URL, "Synthesiser Connector")
+
+	/*** CREATE TWIN THEROMSTAT WITH CONTROL DELEG ***/
+	/* There's no need to create a Twin Model for this exercise.
+	   If there was a need to create more than just a Twin Follower,
+	   then probably a Twin Model would be beneficial.
+	   We don't care abut changing the AllowLists either
+	   (no one needs to find, describe or send data to this Twin). */
+	twinThermostatIdentity := helpers.CreateTwinWithControlDelegation(resolverClient, AGENT_SEED, "Thermostat", agentIdentity)
+
+	_, err = apiContext.TwinAPI.CreateTwin(
+		apiContext.CtxWithMeta,
+		&ioticsApi.CreateTwinRequest{
+			Headers: &apiContext.Headers,
+			Payload: &ioticsApi.CreateTwinRequest_Payload{
+				Id: twinThermostatIdentity.Did(),
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalf("Could not Create Twin: %v", err)
+	}
+	log.Printf("Created Twin %s", twinThermostatIdentity.Did())
+
+	/*** SEARCH FOR TWINS TEMPERATURE ***/
+	/* We need to find the 2 Twins Publisher. To do that we need to define:
+	   1. One or more searching criteria
+	   2. The type of the response
+	   3. The scope of the Search. */
+	headersSearch := ioticsApi.Headers{
+		ClientAppId:    "Iotics Search",
+		TransactionRef: []string{"Search"},
+		RequestTimeout: timestamppb.New(time.Now().Add(time.Second * 3)),
+	}
+
+	searchResponseClient, searchErr := apiContext.SearchAPI.SynchronousSearch(
+		apiContext.CtxWithMeta,
+		&ioticsApi.SearchRequest{
+			Headers: &headersSearch,
+			Scope:   ioticsApi.Scope_GLOBAL,
+			Payload: &ioticsApi.SearchRequest_Payload{
+				ResponseType: ioticsApi.ResponseType_FULL,
+				Filter: &ioticsApi.SearchRequest_Payload_Filter{
+					Properties: []*ioticsApi.Property{
+						{
+							Key: helpers.PROPERTY_KEY_CREATED_BY,
+							Value: &ioticsApi.Property_StringLiteralValue{
+								StringLiteralValue: &ioticsApi.StringLiteral{
+									Value: "Lorenzo Paris",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	if searchErr != nil {
+		log.Fatalf("Could not search: %v", searchErr)
+	}
+
+	var twinPubList []*ioticsApi.SearchResponse_TwinDetails
+
+	for {
+		resp, respErr := searchResponseClient.Recv()
+		if respErr != nil {
+			break
+		}
+		twinPubList = append(twinPubList, resp.GetPayload().GetTwins()...)
+	}
+
+	/*** FOLLOW PUBLISHERS' FEED DATA ***/
+	/* In order to follow a Twin's Feed we need to know:
+	   1. The Twin DID
+	   2. The Host ID of the Twin Pulbisher (if remote)
+	   3. The Feed ID.
+	   However, in order to send Input message we need to know the value of the Input's Label
+	   which can be retrieved only via the DescribeFeed operation. */
+	ch := make(chan bool, len(twinPubList)+1) // BONUS: len(twinPubList) + 1
+
+	for _, twin := range twinPubList {
+		feedOfInterest := twin.GetFeeds()[0]
+
+		feedDescription, err := apiContext.FeedAPI.DescribeFeed(
+			apiContext.CtxWithMeta,
+			&ioticsApi.DescribeFeedRequest{
+				Headers: &apiContext.Headers,
+				Args: &ioticsApi.DescribeFeedRequest_Arguments{
+					FeedId: &ioticsApi.FeedID{
+						Id:     feedOfInterest.FeedId.Id,
+						TwinId: feedOfInterest.FeedId.TwinId,
+						HostId: feedOfInterest.FeedId.HostId,
+					},
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Could not Describe Feed: %v", err)
+		}
+
+		feedValueLabel := feedDescription.GetPayload().Result.GetValues()[0].Label
+
+		feedListener, err := apiContext.InterestAPI.FetchInterests(
+			apiContext.CtxWithMeta,
+			&ioticsApi.FetchInterestRequest{
+				Headers: &apiContext.Headers,
+				Args: &ioticsApi.FetchInterestRequest_Arguments{
+					Interest: &ioticsApi.Interest{
+						FollowerTwinId: &ioticsApi.TwinID{Id: twinThermostatIdentity.Did()},
+						FollowedFeedId: &ioticsApi.FeedID{
+							Id:     feedOfInterest.FeedId.Id,
+							TwinId: feedOfInterest.FeedId.TwinId,
+							HostId: feedOfInterest.FeedId.HostId,
+						},
+					},
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Could not receive Feed messages: %v", err)
+		}
+
+		go getFeedData(feedListener, feedOfInterest.FeedId.TwinId, feedValueLabel, ch)
+	}
+
+	/*** SEARCH FOR TWIN RADIATOR ***/
+	headersSearch = ioticsApi.Headers{
+		ClientAppId:    "Iotics Search",
+		TransactionRef: []string{"Search"},
+		RequestTimeout: timestamppb.New(time.Now().Add(time.Second * 3)),
+	}
+
+	searchResponseClient, searchErr = apiContext.SearchAPI.SynchronousSearch(
+		apiContext.CtxWithMeta,
+		&ioticsApi.SearchRequest{
+			Headers: &headersSearch,
+			Scope:   ioticsApi.Scope_GLOBAL,
+			Payload: &ioticsApi.SearchRequest_Payload{
+				ResponseType: ioticsApi.ResponseType_FULL,
+				Filter: &ioticsApi.SearchRequest_Payload_Filter{
+					Properties: []*ioticsApi.Property{
+						{
+							Key: helpers.PROPERTY_KEY_LABEL,
+							Value: &ioticsApi.Property_LangLiteralValue{
+								LangLiteralValue: &ioticsApi.LangLiteral{
+									Value: "Twin Radiator",
+									Lang:  "en",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	if searchErr != nil {
+		log.Fatalf("Could not search: %v", searchErr)
+	}
+
+	var twinRadiatorList []*ioticsApi.SearchResponse_TwinDetails
+
+	for {
+		resp, respErr := searchResponseClient.Recv()
+		if respErr != nil {
+			break
+		}
+		twinRadiatorList = append(twinRadiatorList, resp.GetPayload().GetTwins()...)
+	}
+
+	/*** BONUS ***/
+	// _, err = apiContext.TwinAPI.UpsertTwin(
+	// 	apiContext.CtxWithMeta,
+	// 	&ioticsApi.UpsertTwinRequest{
+	// 		Headers: &apiContext.Headers,
+	// 		Payload: &ioticsApi.UpsertTwinRequest_Payload{
+	// 			TwinId: &ioticsApi.TwinID{Id: twinThermostatIdentity.Did()},
+	// 			Properties: []*ioticsApi.Property{
+	// 				{
+	// 					Key: helpers.PROPERTY_KEY_LABEL,
+	// 					Value: &ioticsApi.Property_LangLiteralValue{
+	// 						LangLiteralValue: &ioticsApi.LangLiteral{
+	// 							Value: "Twin Thermostat",
+	// 							Lang:  "en",
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 			Inputs: []*ioticsApi.UpsertInputWithMeta{
+	// 				{
+	// 					Id: "presence",
+	// 					Values: []*ioticsApi.Value{
+	// 						{
+	// 							Label:    "presence_detected",
+	// 							Comment:  "Whether the sensor detected a presence",
+	// 							DataType: "boolean",
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// )
+	// if err != nil {
+	// 	log.Fatalf("Could not Upsert Twin: %v", err)
+	// }
+
+	// inputListener, rcvErr := apiContext.InputAPI.ReceiveInputMessages(
+	// 	apiContext.CtxWithMeta,
+	// 	&ioticsApi.ReceiveInputMessageRequest{
+	// 		Headers: &apiContext.Headers,
+	// 		Args: &ioticsApi.ReceiveInputMessageRequest_Arguments{
+	// 			InputId: &ioticsApi.InputID{
+	// 				Id:     "presence",
+	// 				TwinId: twinThermostatIdentity.Did(),
+	// 			},
+	// 		},
+	// 	},
+	// )
+	// if rcvErr != nil {
+	// 	log.Fatalf("Could not receive Input messages: %v", rcvErr)
+	// }
+
+	// go getInputMessages(inputListener, "presence_detected", ch)
+
+	/*** THERMOSTAT LOGIC ***/
+	statusOn := false
+	var dataToShare []byte
+	for {
+		turnOn := true
+
+		for i := 0; i < len(twinPubList); i++ { // BONUS: i < len(twinPubList)+1
+			turnOn = turnOn && <-ch
+		}
+
+		if turnOn && !statusOn {
+			inputMessage := `{"turn_on": true}`
+			dataToShare = []byte(inputMessage)
+
+			_, err := apiContext.InterestAPI.SendInputMessage(
+				apiContext.CtxWithMeta,
+				&ioticsApi.SendInputMessageRequest{
+					Headers: &apiContext.Headers,
+					Args: &ioticsApi.SendInputMessageRequest_Arguments{
+						Interest: &ioticsApi.InputInterest{
+							SenderTwinId: &ioticsApi.TwinID{Id: twinThermostatIdentity.Did()},
+							DestInputId: &ioticsApi.InputID{
+								Id:     twinRadiatorList[0].GetInputs()[0].InputId.Id,
+								TwinId: twinRadiatorList[0].GetInputs()[0].InputId.TwinId,
+							},
+						},
+					},
+					Payload: &ioticsApi.SendInputMessageRequest_Payload{
+						Message: &ioticsApi.InputMessage{
+							Data: dataToShare,
+							Mime: "application/json",
+						},
+					},
+				},
+			)
+			if err != nil {
+				log.Fatalf("Could not send Input Message: %v", err)
+			}
+			log.Printf("Sending input message %s", inputMessage)
+
+			statusOn = true // Update radiator's status
+		} else if !turnOn && statusOn {
+			inputMessage := `{"turn_on": false}`
+			dataToShare = []byte(inputMessage)
+
+			_, err := apiContext.InterestAPI.SendInputMessage(
+				apiContext.CtxWithMeta,
+				&ioticsApi.SendInputMessageRequest{
+					Headers: &apiContext.Headers,
+					Args: &ioticsApi.SendInputMessageRequest_Arguments{
+						Interest: &ioticsApi.InputInterest{
+							SenderTwinId: &ioticsApi.TwinID{Id: twinThermostatIdentity.Did()},
+							DestInputId: &ioticsApi.InputID{
+								Id:     twinRadiatorList[0].GetInputs()[0].InputId.Id,
+								TwinId: twinRadiatorList[0].GetInputs()[0].InputId.TwinId,
+							},
+						},
+					},
+					Payload: &ioticsApi.SendInputMessageRequest_Payload{
+						Message: &ioticsApi.InputMessage{
+							Data: dataToShare,
+							Mime: "application/json",
+						},
+					},
+				},
+			)
+			if err != nil {
+				log.Fatalf("Could not send Input Message: %v", err)
+			}
+			log.Printf("Sending input message %s", inputMessage)
+
+			statusOn = false // Update radiator's status
+		}
+	}
+
+}
