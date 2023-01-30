8efb1947a44dd10a375ed209a2497e1fdd27f9d0
Lorenzo Paris <lorenzo.paris@iotics.com>
2023-01-30 16:08:35 +0000

    Added golang skeleton + changed allow all/none with new values

---
+++ golang/cmd/twinReceiver/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"iotics-training/pkg/helpers"
+	"log"
+	"net/url"
+
+	ioticsApi "github.com/Iotic-Labs/iotics-api-grpc-go/iotics/api"
+	"github.com/Iotic-Labs/iotics-identity-go/pkg/register"
+)
+
+const (
+	RESOLVER_URL  = ""
+	HOST_GRPC_URL = "" // <spacename>.iotics.com:10001
+
+	USER_KEY_NAME = ""
+	USER_SEED     = ""
+
+	AGENT_KEY_NAME = ""
+	AGENT_SEED     = ""
+)
+
+func main() {}
---
+++ golang/pkg/helpers/const.go
@@ -0,0 +1,26 @@
+package helpers
+
+const (
+	PROPERTY_KEY_HOST_METADATA_ALLOW_LIST = "http://data.iotics.com/public#hostMetadataAllowList"
+	PROPERTY_KEY_HOST_ALLOW_LIST          = "http://data.iotics.com/public#hostAllowList"
+
+	PROPERTY_KEY_LABEL      = "http://www.w3.org/2000/01/rdf-schema#label"
+	PROPERTY_KEY_COMMENT    = "http://www.w3.org/2000/01/rdf-schema#comment"
+	PROPERTY_KEY_TYPE       = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+	PROPERTY_KEY_FROM_MODEL = "https://data.iotics.com/app#model"
+	PROPERTY_KEY_COLOR      = "https://data.iotics.com/app#color"
+	PROPERTY_KEY_SPACE_NAME = "https://data.iotics.com/app#spaceName"
+	PROPERTY_KEY_CREATED_BY = "https://data.iotics.com/app#createdBy"
+	PROPERTY_KEY_DEFINES    = "https://data.iotics.com/app#defines"
+
+	PROPERTY_KEY_SAREF_HAS_MODEL = "https://saref.etsi.org/core/hasModel"
+
+	PROPERTY_VALUE_ALLOW_ALL  = "http://data.iotics.com/public#all"
+	PROPERTY_VALUE_ALLOW_NONE = "http://data.iotics.com/public#none"
+
+	PROPERTY_VALUE_MODEL = "https://data.iotics.com/app#Model"
+
+	PROPERTY_VALUE_SAREF_TEMPERATURE_SENSOR = "https://saref.etsi.org/core/TemperatureSensor"
+
+	UNIT_DEGREE_CELSIUS = "http://qudt.org/vocab/unit/DEG_C"
+)
